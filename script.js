30f5703031f42f09b4fa40752ab8e72f5719615d
Carlos.Ernesto.A <calcanta_peru@hotmail.com>
2023-06-06 14:45:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// change Login to Logout when clicked
+function flogin(elemento) {
+
+
+    elemento.innerText = "Logout";
+
+
+
+}
+
+
+// Alert Ninja was liked when clicked
+function flike() {
+
+    alert("Ninja was liked");
+
+}
+
+// Remove Add Definition Button when clicked
+function fadddef(elemento) {
+
+    elemento.remove();
+
+
+
+}
